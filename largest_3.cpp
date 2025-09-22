5d0c271117b8f77bc000664ee75feb574d89f275
Akshata6524 <nagalotiakshata@gmail.com>
2025-09-22 22:44:07 +0530

    Create largest_3.cpp

---
+++ largest_3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter the values for a b and c:";
+    cin>>a>>b>>c;
+    if(a>=b&&a>=c){
+        cout<<a<<" is largest";
+    }
+    else if (b>=a&&b>=c){
+        cout<<b<<" is largest";
+    }
+    else 
+    cout<<c<<" is largest";
+}
