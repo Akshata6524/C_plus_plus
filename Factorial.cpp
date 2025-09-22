7965c4044203f1655249eb9e95641982aa734d22
Akshata6524 <nagalotiakshata@gmail.com>
2025-09-22 23:20:39 +0530

    Update Factorial.cpp

--- Factorial.cpp
+++ Factorial.cpp
@@ -15,3 +15,20 @@ int main(){
     fact=factorial(n);
     cout<<"Factorial of "<<n<<" is "<<fact;
 }
+
+//factorial using recursive
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    if(n==0||n==1)
+    return 1;
+    else 
+    return n*factorial(n-1);
+}
+int main(){
+    int n,fact;
+    cout<<"Enter the value for n:";
+    cin>>n;
+    fact=factorial(n);
+    cout<<"Factorial of "<<n<<" is "<<fact;
+}
