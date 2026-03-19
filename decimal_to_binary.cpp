d28b5b0a1ffce3879b6100c926448aeda2d5cbb3
Akshata6524 <nagalotiakshata@gmail.com>
2026-03-19 20:13:35 +0530

    Update decimal_to_binary.cpp

--- decimal_to_binary.cpp
+++ decimal_to_binary.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
- int p=1,ans=0;
+ int i=0,ans=0;
  
  int  decimaltobinary(int dec_num){
      
      while(dec_num>0){
        int rem=dec_num%2;
         dec_num=dec_num/2;
-        ans=ans+(rem*p);
-       p=p*10;
+        ans=ans+(rem*(pow(10,i)));
+       i++;
      }
      return ans;
  }
