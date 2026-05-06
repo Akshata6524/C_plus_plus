b25df587ef99eac80cd0516eb11e8861dd4031b7
Akshata6524 <nagalotiakshata@gmail.com>
2026-05-06 15:11:51 +0530

    Fix reverse array logic and variable initialization

--- reverse_array.cpp
+++ reverse_array.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[10],i,n;
+    int arr[10],i,n,low,high;
     cout<<"Enter the array size:";
     cin>>n;
-    int low=0,high=n-1;
+    low=0;
+    high=n-1;
     cout<<"Enter the array elements:";
     for(i=0;i<n;i++)
     cin>>arr[i];
-    int mid=(low+high)/2;
-    while(low!=mid){
+    while(low<high){
         swap(arr[low],arr[high]);
         low++;
         high--;
